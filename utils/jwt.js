8767c5e198fdfc7958b90fa53b63e1d2fbffb400
Jyotikeshri <156110025+Jyotikeshri@users.noreply.github.com>
2024-12-22 14:35:24 +0530

    4

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,54 +1,66 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const accessTokenExpire = parseInt(
+const accessTokenExpire = parseInt(
   process.env.ACCESS_TOKEN_EXPIRE || "300",
   10
 );
-export const refreshTokenExpire = parseInt(
+const refreshTokenExpire = parseInt(
   process.env.REFRESH_TOKEN_EXPIRE || "1200",
   10
 );
 
+//option for cookies
 export const accessTokenOptions = {
-  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000), // 10 minutes in milliseconds
-  maxAge: accessTokenExpire * 60 * 60 * 1000, // Same as above
+  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // Secure in production
+  sameSite: "lax",
+};
+export const refreshTokenOptions = {
+  expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+  httpOnly: true,
   sameSite: "lax",
 };
 
-export const refreshTokenOptions = {
-  expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000), // 40 minutes in milliseconds
-  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000, // Same as above
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // Secure in production
-  sameSite: "lax",
-};
+// export const accessTokenOptions = {
+//   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000), // 10 minutes in milliseconds
+//   maxAge: accessTokenExpire * 60 * 60 * 1000, // Same as above
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production", // Secure in production
+//   sameSite: "lax",
+// };
+
+// export const refreshTokenOptions = {
+//   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000), // 40 minutes in milliseconds
+//   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000, // Same as above
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production", // Secure in production
+//   sameSite: "lax",
+// };
 
 export const sendToken = async (user, statusCode, res) => {
-  const accessToken = await user.signAccessToken();
-  const refreshToken = await user.signRefreshToken();
+  const accessToken = user.SignAccessToken();
+  const refreshToken = user.SignRefreshToken();
 
-  console.log(user, " ", statusCode, " ", refreshToken);
+  // upload session to redis
+  console.log("accessToken: " + accessToken);
+  console.log("refreshToken: " + refreshToken);
 
-  // No Redis operation here, all the data is stored and fetched directly from the database
-
-  // Ensure the cookies are secure in production
+  // only set secure to true in production
   if (process.env.NODE_ENV === "production") {
     accessTokenOptions.secure = true;
   }
 
-  // Set the cookies with the access and refresh tokens
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+  console.log("cookies: " + res.cookie.access_token);
+  console.log("cookies: " + res.cookie.refresh_token);
 
-  console.log("res.cookie", res.cookie);
-
-  // Send the response
   res.status(statusCode).json({
     success: true,
     user,
-    accessToken: accessToken,
+    accessToken,
   });
 };
